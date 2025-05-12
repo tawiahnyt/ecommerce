f6e62968c07511cf45ebd5fe86b5516aeeb5ea55
tawiahnyt <tawiahin4k@gmail.com>
2025-05-12 09:51:56 +0000

    feat: add dotenv for environment variable management and set up authentication routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, {Express, Request, Response} from 'express';
+import express, {Express} from 'express';
+import { PORT } from './secrets';
+import rootRouter from './routes';
 
 
 const app:Express = express();
 
-app.get('/', (req:Request, res:Response) => {
-    res.send('Working!');
-})
+app.use('/api', rootRouter)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
